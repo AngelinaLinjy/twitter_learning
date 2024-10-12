86c69c13460c34c2e92fc2cd889326a7b1027f59
Angelina Lin <angelina_ljy@163.com>
2024-10-12 14:54:41 +0800

    feat: extract post line to live component with new ui

---
+++ lib/auth_learning_web/live/post_live/post_component.ex
@@ -0,0 +1,50 @@
+defmodule AuthLearningWeb.PostLive.PostComponent do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~H"""
+    <div class="tweet flex">
+      <img src="" alt="avatar" class="avatar" />
+      <div class="w-full">
+        <div class="tweet-header">
+          <strong>@<%= @post.user.name %></strong>
+          <span class="tweet-timestamp"><%= format_timestamp(@post.inserted_at) %></span>
+        </div>
+
+        <div class="tweet-body">
+          <p><%= @post.body %></p>
+        </div>
+        <div class="tweet-footer">
+          <button phx-click="like">❤️ Like</button>
+          <%!-- <span><%= @post.likes_count %> Likes</span> --%>
+          <button phx-click="retweet">🔁 Retweet</button>
+          <button phx-click="edit">✏️ Edit</button>
+          <button phx-click="delete">🚮 Delete</button>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp format_timestamp(timestamp) do
+    current_time = DateTime.utc_now()
+    elapsed_time = DateTime.diff(current_time, timestamp)
+
+    cond do
+      elapsed_time < 60 ->
+        "#{elapsed_time} second#{if elapsed_time != 1, do: "s"} ago"
+
+      elapsed_time < 3600 ->
+        minutes = div(elapsed_time, 60)
+        "#{minutes} minute#{if minutes != 1, do: "s"} ago"
+
+      elapsed_time < 86400 ->
+        hours = div(elapsed_time, 3600)
+        "#{hours} hour#{if hours != 1, do: "s"} ago"
+
+      true ->
+        days = div(elapsed_time, 86400)
+        "#{days} day#{if days != 1, do: "s"} ago"
+    end
+  end
+end
