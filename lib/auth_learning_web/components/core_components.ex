4e4ead0968b905fc6918a98cfaa12889dcef5e6e
Angelina Lin <angelina_ljy@163.com>
2024-10-21 22:36:24 +0800

    fix: text white

--- lib/auth_learning_web/components/core_components.ex
+++ lib/auth_learning_web/components/core_components.ex
@@ -384,7 +384,7 @@ defmodule AuthLearningWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 bg-gray-900",
+          "mt-2 block w-full rounded-lg text-white focus:ring-0 sm:text-sm sm:leading-6 bg-gray-900",
           @errors == [] && "border-gray-800 focus:border-gray-600",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
